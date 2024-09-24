86059cafba75cb5a74cecc8bc7c908848a6b6944
tommipiz <tommaso.pizzutilo@gmail.com>
2024-09-24 12:33:22 +0200

    Completo

--- es3_GrandezzaFont.php
+++ es3_GrandezzaFont.php
@@ -7,8 +7,10 @@
 </head>
 <body>
     <?php   
+        $grandezza = rand(16,60) . 'px';
+        echo "<p>La grandezza è $grandezza</p>";
 
-
+        echo "<p style='font-size:$grandezza'>Paragrafo la cui grandezza del font in pixel è un numero casuale compreso tra 16 e 60</p>";
 
     ?>
     
